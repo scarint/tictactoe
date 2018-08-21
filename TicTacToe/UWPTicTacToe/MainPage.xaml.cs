bc12e467392f790d98047642c484f383eb8654ef
scarint <michaelpballard@gmail.com>
2018-08-20 21:02:37 -0700

    UWP Interface and basic selection

--- TicTacToe/UWPTicTacToe/MainPage.xaml.cs
+++ TicTacToe/UWPTicTacToe/MainPage.xaml.cs
@@ -22,9 +22,32 @@ namespace UWPTicTacToe
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        TextBlock selected;
+        Thickness show = new Thickness(5, 5, 5, 5);
+        Thickness hide = new Thickness(0, 0, 0, 0);
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void BoxSelectionChanged(object sender, PointerRoutedEventArgs e)
+        {
+            // Assign sender to the box that was clicked
+            TextBlock clicked = (TextBlock)sender;
+
+            // Check border
+            if (Box00Border.BorderThickness == hide)                        // If selection border is hidden
+            {
+                Box00Border.BorderThickness = show;                         // Show it and set our selected values
+                selected = Box00;
+            }
+            else                                                            // Otherwise, it's already visible
+            {
+                Box00Border.BorderThickness = hide;                         // So "delete" it, and
+                Box00.Text = "X";                                           // set the TextBlock,
+                selected = null;                                            // and  clear selected
+            }
         }
     }
 }
